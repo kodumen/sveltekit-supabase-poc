0a2474bd9cd1a2c3296b66cf47ab15b69351000c
rdaitan <daitan@greenova.de>
2022-01-05 17:11:15 +0800

    Add TODO

--- src/lib/load/auth.ts
+++ src/lib/load/auth.ts
@@ -3,9 +3,13 @@ import {browser} from "$app/env";
 import supabase from "$lib/supabase";
 
 export const guest = async (input:LoadInput):Promise<LoadOutput> => {
+    // TODO: This check is not yet working on the server side. Make it work.
+    const req = {cookies: input.session.cookies};
     const user = browser ?
         supabase.auth.user() :
-        (await supabase.auth.api.getUserByCookie(input.session.cookies)).user;
+        (await supabase.auth.api.getUserByCookie(req)).user;
+
+    console.log(browser, req, user);
 
     if (user === null) {
         return null
