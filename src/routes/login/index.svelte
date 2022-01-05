434be547a715b982b44bc2b90f3a1f7df1b943b8
rdaitan <daitan@greenova.de>
2022-01-05 13:54:08 +0800

    Implement login
    We can call the supabase API from the browser. Pretty neat. Access
    token is stored in local storage.

---
+++ src/routes/login/index.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import supabase from "$lib/supabase";
+    import {goto} from "$app/navigation";
+
+    let errorMessage = ''
+
+    export async function handleSubmit(event: Event): Promise<void> {
+        event.preventDefault();
+
+        const body = new FormData(event.target as HTMLFormElement);
+
+        const {error} = await supabase.auth.signIn({
+            email: body.get('email') as string,
+            password: body.get('password') as string
+        });
+
+        if (error) {
+            errorMessage = error.message
+        } else {
+            goto('/')
+        }
+    }
+</script>
+
+<section>
+    <form method="post" on:submit|preventDefault={handleSubmit}>
+        <h1>Login</h1>
+        {#if errorMessage}
+            <div>{errorMessage}</div>
+        {/if}
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</section>
